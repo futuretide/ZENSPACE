0d173db0b7fc54bc12156f2c72f47302179c7486
programmerAI <56351627+harsha6456@users.noreply.github.com>
2019-10-10 20:04:36 +0530

    Arduino basic LED in_out

--- LED_basic_1.ino
+++ LED_basic_1.ino
@@ -1,21 +1,21 @@
 int LedA = 13;
 int LedB = 11;
 int LedC = 9;
-
+//basic location of pin//
 void Led(int a, int b, int c) {
   digitalWrite(LedA, a);
   digitalWrite(LedB, b);
   digitalWrite(LedC, c);
 }
-
+//pin input//
 void setup() {
   pinMode(LedA, OUTPUT);
   pinMode(LedB, OUTPUT);
   pinMode(LedC, OUTPUT);
 }
-
+// LED function in the below code//
 void loop() {
-  Led(0, 0, 1);
+  Led(0, 1, 1);
   delay(1000);
 
   Led(0, 1, 0);
@@ -23,15 +23,17 @@ void loop() {
 
   Led(0, 0, 1);
   delay(1000);
+  
+  Led (0 ,0, 0,);
+  delay (1000)
 
-
-  Led(1,1 , 1);
+  Led(1,1,1);
   delay(1000);
 
   Led(0, 1, 1);
   delay(1000);
 
-  Led(0,1,0);
+  Led(0 ,1, 0);
   delay(1000);
 
   
